bc0acbe36bce67f60ed5ad458fb30ff82be6bb4c
nishanthkumarg <nishanthkumarg@google.com>
2016-11-04 11:43:22 -0700

    Lint Check Packaging error

---
+++ app/src/main/java/com/example/nishanthkumarg/myapplication/lintpackage/WithInPackage.java
@@ -0,0 +1,15 @@
+package com.example.nishanthkumarg.myapplication.lintpackage;
+
+/**
+ * Created by nishanthkumarg on 11/4/16.
+ */
+
+public class WithInPackage {
+
+    Lintcheck lc = new Lintcheck();
+
+    WithInPackage(){
+        lc.sampleFunction();
+    }
+
+}
